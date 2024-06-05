3f346cf101879d72d7fe6a32df5feab7c35f5fb4
Chris <48462986+turnerdev@users.noreply.github.com>
2024-06-05 13:25:13 -0700

    scanners

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "crafting interpreters tree-walk interpreter";
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ jdk google-java-format ];
+          };
+        });
+    };
+}
