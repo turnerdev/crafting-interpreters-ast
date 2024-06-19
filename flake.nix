463f6714d99ac9dba768f5e3c6f3caf10bdc398d
Chris <48462986+turnerdev@users.noreply.github.com>
2024-06-19 08:49:05 -0700

    representing code

--- flake.nix
+++ flake.nix
@@ -8,12 +8,41 @@
         nixpkgs.lib.genAttrs supportedSystems (system: f system);
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in {
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          lox = pkgs.stdenv.mkDerivation {
+            pname = "Lox";
+            version = "1.0";
+            buildInputs = with pkgs; [ jdk ];
+            src = ./.;
+            buildPhase = ''
+              javac -d $out com/craftinginterpreters/lox/Lox.java
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cat > $out/bin/lox <<EOF
+              #!${pkgs.stdenv.shell}
+              exec ${pkgs.jdk}/bin/java -cp "$out" com.craftinginterpreters.lox.Lox "\$@"
+              EOF
+              chmod +x $out/bin/lox
+            '';
+          };
+        });
+
       devShells = forAllSystems (system:
         let pkgs = nixpkgsFor.${system};
         in {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [ jdk google-java-format ];
+            buildInputs = with pkgs; [
+              jdk
+              google-java-format
+              self.packages.${system}.lox
+            ];
           };
         });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.lox);
     };
 }
